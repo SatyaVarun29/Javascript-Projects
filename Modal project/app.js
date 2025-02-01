ca93b25a86f0aa18519813739c034ebceb66d44d
SatyaVarun29 <satyavarun87@gmail.com>
2025-02-01 12:09:27 +0530

    add changes

--- Modal project/app.js
+++ Modal project/app.js
@@ -2,3 +2,15 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modelbtn=document.querySelector('.modal-btn')
+const modeloverlay=document.querySelector('.modal-overlay')
+const closebtn=document.querySelector('.close-btn')
+
+modelbtn.addEventListener('click',()=>{
+    modeloverlay.classList.add('open-modal')
+})
+
+closebtn.addEventListener('click',()=>{
+    modeloverlay.classList.remove('open-modal')
+})
