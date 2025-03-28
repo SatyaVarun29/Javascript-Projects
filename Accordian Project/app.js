99c2043e93ce10d13e47f9676c9cc9a4032c3a9f
SatyaVarun29 <satyavarun87@gmail.com>
2025-03-28 10:49:05 +0530

    add

--- Accordian Project/app.js
+++ Accordian Project/app.js
@@ -1,36 +1,34 @@
 //using selectors inside the element
 
+
 const questions=document.querySelectorAll('.question')
-questions.forEach((question)=>{
-    const btn=question.querySelector('.question-btn')
-btn.addEventListener('click',()=>{
+questions.forEach((question) => {
+  // console.log(question);
+  const btn = question.querySelector(".question-btn");
 
-    questions.forEach(function(item){
-      if(item!==question){
-        item.classList.remove('show-text')
-      }
-    })
-
-    question.classList.toggle('show-text')
-})
-    
-
+  btn.addEventListener("click", () => {
+    questions.forEach((item)=>{
+      // console.log(item);
+      
+if(item!==question){
+  item.classList.remove('show-text')
 }
-)
 
 
-
-
-
-
-
+    })
+    question.classList.toggle("show-text");
+  });
+});
 
 // traversing the dom
-// const btns=document.querySelectorAll('.question-btn')
-// btns.forEach((btn)=>{
-// btn.addEventListener('click',function(e){
-// const question=e.currentTarget.parentElement.parentElement
-// question.classList.toggle('show-text')
-
-// })
-// })
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach(function (btn) {
+//   btn.addEventListener("click", (e) => {
+//     const answer = e.currentTarget.parentElement.parentElement;
+//     if (!answer.classList.contains("show-text")) {
+//       answer.classList.add("show-text");
+//     } else {
+//       answer.classList.remove("show-text");
+//     }
+//   });
+// });
